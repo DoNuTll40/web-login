b8b2e2e35c37eaf935488df74ad4496aefed7fbc
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-09-10 07:48:58 +0700

    update code v0.0124

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const ipAddress = location.hostname;
 
-axios.defaults.baseURL = `https://api.donut-ll40.me`;
+axios.defaults.baseURL = `https://elysia-api-mongodb.onrender.com`;
 axios.defaults.withCredentials = true
 
 export default axios;
